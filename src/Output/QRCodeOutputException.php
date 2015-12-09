d74ac931066386661c2ba9aa11db281b41e5f8c3
smiley <smiley@chillerlan.net>
2015-12-09 04:20:40 +0100

    added QRCodeOutputException

---
+++ src/Output/QRCodeOutputException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class QRCodeOutputException
+ *
+ * @filesource   QRCodeOutputException.php
+ * @created      09.12.2015
+ * @package      chillerlan\QRCode
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2015 Smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\QRCode\Output;
+
+use Exception;
+
+/**
+ * Placeholder
+ */
+class QRCodeOutputException extends Exception{
+
+}
