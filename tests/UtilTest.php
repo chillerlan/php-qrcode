69acabc2a6113d397b5dc6492f58facb8c9a9383
smiley <smiley@chillerlan.net>
2016-02-08 15:47:44 +0100

    "compiles on my machine"

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -12,19 +12,20 @@ use chillerlan\QRCode\Util;
 class UtilTest extends PHPUnit_Framework_TestCase{
 
 	public function testIsNumber(){
-		$this->assertEquals(true,  Util::isNumber('1234567890'));
+		$this->assertEquals(true, Util::isNumber('1234567890'));
 		$this->assertEquals(false, Util::isNumber('abc'));
 	}
 
 	public function testIsAlphaNum(){
-		$this->assertEquals(true,  Util::isAlphaNum('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 $%*+-./:'));
+		$this->assertEquals(true, Util::isAlphaNum('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 $%*+-./:'));
 		$this->assertEquals(false, Util::isAlphaNum('#'));
 	}
 
 	public function testIsKanji(){
 		$this->assertEquals(true,  Util::isKanji('茗荷'));
-		$this->assertEquals(false, Util::isKanji('茗'));
-		$this->assertEquals(false, Util::isKanji('#'));
+		$this->assertEquals(false, Util::isKanji(''));
+		$this->assertEquals(false, Util::isKanji('ÃÃÃ')); // non-kanji
+		$this->assertEquals(false, Util::isKanji('荷')); // kanji forced into byte mode due to length
 	}
 
 }
