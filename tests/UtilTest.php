b1a67d6cd311d321da973a4080e3cc9ec858adf9
smiley <smiley@chillerlan.net>
2016-02-09 16:47:16 +0100

    more tests

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -9,6 +9,8 @@
 
 namespace chillerlan\QRCodeTest;
 
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QRConst;
 use chillerlan\QRCode\Util;
 
 class UtilTest extends \PHPUnit_Framework_TestCase{
@@ -29,6 +31,35 @@ class UtilTest extends \PHPUnit_Framework_TestCase{
 		$this->assertEquals(false, Util::isKanji(''));
 		$this->assertEquals(false, Util::isKanji('ÃÃÃ')); // non-kanji
 		$this->assertEquals(false, Util::isKanji('荷')); // kanji forced into byte mode due to length
+	}
+
+	// coverage
+	public function testGetBCHTypeNumber(){
+		$this->assertEquals(7973, Util::getBCHTypeNumber(1));
+	}
+
+	/**
+	 * @expectedException \chillerlan\QRCode\QRCodeException
+	 * @expectedExceptionMessage $typeNumber: 1 / $errorCorrectLevel: 42
+	 */
+	public function testGetRSBlocksException(){
+		Util::getRSBlocks(1, 42);
+	}
+
+	/**
+	 * @expectedException \chillerlan\QRCode\QRCodeException
+	 * @expectedExceptionMessage Invalid error correct level: 42
+	 */
+	public static function testGetMaxLengthECLevelException(){
+		Util::getMaxLength(QRCode::TYPE_01, QRConst::MODE_BYTE, 42);
+	}
+
+	/**
+	 * @expectedException \chillerlan\QRCode\QRCodeException
+	 * @expectedExceptionMessage Invalid mode: 1337
+	 */
+	public static function testGetMaxLengthModeException(){
+		Util::getMaxLength(QRCode::TYPE_01, 1337, QRCode::ERROR_CORRECT_LEVEL_H);
 	}
 
 }
