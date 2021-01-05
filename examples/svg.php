be3beb936c21fe53a4e7e8f7f3582e9f02443666
codemasher <smiley@chillerlan.net>
2021-01-05 22:21:28 +0100

    :shower: fix svg example

--- examples/svg.php
+++ examples/svg.php
@@ -20,6 +20,7 @@ $gzip = true;
 $options = new QROptions([
 	'version'      => 7,
 	'outputType'   => QRCode::OUTPUT_MARKUP_SVG,
+	'imageBase64'  => false,
 	'eccLevel'     => QRCode::ECC_L,
 	'svgViewBoxSize' => 530,
 	'addQuietzone' => true,
